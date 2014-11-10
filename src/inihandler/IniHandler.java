a406f7e12919b96562ecc63f53fbad715ea39b4b
GlasFrost <lthartmann1@gmail.com>
2014-11-10 15:29:53 +0100

    Initial commit

---
+++ src/inihandler/IniHandler.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inihandler;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * handles ini files
+ * @author hartmann_luis
+ */
+public class IniHandler {
+    
+    /* Map of content of ini file */
+    public Map<String, Map<String, String>> cfg = new HashMap();
+    
+    /* filename of ini file */
+    private String filename = "";
+
+    ////////// PUBLIC FUNCTIONS //////////
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("IniHandler is not supposed to be started as a stand-alone application.");
+    }
+    
+    /**
+     * constructor
+     * @param newfilename 
+     */
+    public void IniHandler(String newfilename) {
+        setFilename(newfilename);
+        reload();
+    }
+    
+    /**
+     * set / change filename
+     * @param newfilename
+     */
+    public void setFilename(String newfilename) {
+        filename = newfilename;
+    }
+    
+    /**
+     * reload the file
+     * @return (boolean) success
+     */
+    public boolean reload() {
+        return loadFile();
+    }
+    
+    /**
+     * save 
+     * @return 
+     */
+    public boolean save() {
+        boolean success = true;
+        BufferedWriter writer = null;
+        try {
+            File iniFile = new File(filename);
+
+            // This will output the full path where the file will be written to...
+            Logger.getLogger(IniHandler.class.getName()).log(Level.INFO, null, iniFile.getCanonicalPath());
+
+            writer = new BufferedWriter(new FileWriter(iniFile));
+            
+            // loop through cfg
+            for (Entry<String, Map<String, String>> entry : cfg.entrySet()) {
+                String header = entry.getKey();
+                Map<String, String> values = entry.getValue();
+                
+                //write section
+                writer.write("["+header+"]");
+                writer.newLine();
+                for(Entry<String, String> keyValuePair : values.entrySet()) {
+                    writer.write(keyValuePair.getKey()+"="+keyValuePair.getValue());
+                    writer.newLine();
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(IniHandler.class.getName()).log(Level.SEVERE, null, ex);
+            success = false;
+        } finally {
+            try {
+                // Close the writer regardless of what happens...
+                writer.close();
+            } catch (Exception ex) {
+                Logger.getLogger(IniHandler.class.getName()).log(Level.INFO, null, ex);
+            }
+        }
+        return success;
+    }
+    
+    
+    ////////// PRIVATE FUNCTIONS //////////
+    
+    /**
+     * read file and call parser
+     * @param filename
+     * @return (boolean) success
+     */
+    private boolean loadFile() {
+        Path path = Paths.get(filename);
+        try {
+            List<String> lines = Files.readAllLines(path, Charset.forName("UTF-8"));
+            parseLines(lines);
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(IniHandler.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+    /**
+     * parser
+     * @param lines 
+     */
+    private void parseLines(List<String> lines) {
+        cfg.clear();
+        String header = "";
+        
+        int line_counter = 1;
+        for(String line : lines) {
+            //check if line is a comment
+            if(line.startsWith("#") || line.startsWith(";")) {
+                //line is a comment: ignore
+                continue;
+            }
+            
+            //check if line is header
+            if(line.startsWith("[") && line.endsWith("]")) {
+                //line is a header
+                header = line.replace("[", "").replace("]", "");
+                continue;
+            }
+            
+            //check if line is a valid key/value pair
+            if(line.contains("=")) {
+                //line is a valid key/value pair
+                String[] keyValuePair = line.split("=",2);
+                HashMap<String, String> helper = new HashMap();
+                if(cfg.containsKey(keyValuePair[0])) {
+                    helper = (HashMap<String, String>) cfg.get(header);
+                }
+                helper.put(keyValuePair[0], keyValuePair[1]);
+                cfg.put(header, helper);
+            }
+            else {
+                //line is not a valid key/value pair: ignore the line
+                RuntimeException ex = new RuntimeException("Ignoring invalid line in config file (Line "+line_counter+")");
+                Logger.getLogger(IniHandler.class.getName()).log(Level.WARNING, null, ex);
+            }
+            line_counter++;
+        }
+    }
+    
+}
